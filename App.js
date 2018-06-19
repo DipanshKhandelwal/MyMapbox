b2321ba9106e3f4de33c8ce3c1e645b3c0761e0f
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-06-19 18:08:50 +0530

    Added simple map

--- App.js
+++ App.js
@@ -1,38 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Mapbox from '@mapbox/react-native-mapbox-gl';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+Mapbox.setAccessToken('YOUR ACCESS TOKEN');
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component<{}> {
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Mapbox.MapView
+            styleURL={Mapbox.StyleURL.Light}
+            zoomLevel={15}
+            centerCoordinate={[77.326017, 28.370669]}
+            style={styles.container}> 
+        </Mapbox.MapView>
       </View>
     );
   }
@@ -41,18 +23,5 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
